c040f35a6fd684c74b4a4b0fb037f41db9fe9312
kazakago <kazakago@gmail.com>
2025-01-25 14:43:17 +0900

    apply default proguard rules

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -71,6 +71,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
