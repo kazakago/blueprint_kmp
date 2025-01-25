049941455313fe105a91f716f8ea76f876345cd4
kazakago <kazakago@gmail.com>
2025-01-25 14:24:05 +0900

    migrate material3 from material2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -40,8 +40,8 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
             implementation(compose.ui)
+            implementation(compose.material3)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
         }
