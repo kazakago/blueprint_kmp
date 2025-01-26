d5e7e2ba9db7b634b65deb29ab5aae0a8ba8d830
kazakago <kazakago@gmail.com>
2025-01-26 21:55:25 +0900

    fix

--- modules/ui/build.gradle.kts
+++ modules/ui/build.gradle.kts
@@ -17,16 +17,9 @@ kotlin {
         }
     }
 
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     jvm("desktop")
 
